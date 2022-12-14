fb677ec390a3d4a73bc922a5857eabea589cf33e
Nabina-shaik <119795935+Nabina-shaik@users.noreply.github.com>
2022-12-14 19:19:32 +0530

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,55 @@
+function addition(){
+    let n1;
+   n1 = document.getElementById("num1").value;
+   n1=Number(n1);console.log(typeof(n1));
+    
+    let n2;
+   n2 = document.getElementById("num2").value;
+   n2=Number(n2);console.log(typeof(n2));
+    
+    let n=n1+n2;
+document.getElementById("add").innerHTML=n;
+}
+
+function subtraction(){
+    let n1;
+   n1 = document.getElementById("num1").value;
+   n1=Number(n1);console.log(typeof(n1));
+    
+    let n2;
+   n2 = document.getElementById("num2").value;
+   n2=Number(n2);console.log(typeof(n2));
+    
+    let n=n1-n2;
+document.getElementById("sub").innerHTML=n;
+}
+
+function multiplication(){
+    let n1;
+   n1 = document.getElementById("num1").value;
+   n1=Number(n1);console.log(typeof(n1));
+    
+    let n2;
+   n2 = document.getElementById("num2").value;
+   n2=Number(n2);console.log(typeof(n2));
+    
+    let n=n1*n2;
+document.getElementById("multi").innerHTML=n;
+}
+
+function division(){
+    let n1;
+   n1 = document.getElementById("num1").value;
+   n1=Number(n1);console.log(typeof(n1));
+    
+    let n2;
+   n2 = document.getElementById("num2").value;
+   n2=Number(n2);console.log(typeof(n2));
+    
+    let n=n1/n2;
+document.getElementById("div").innerHTML=n;
+}
+
+
+        
+    
